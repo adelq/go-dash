8664658f225982cd152f042af61656129a17105d
Adel Qalieh <aqalieh95@gmail.com>
2014-09-27 00:29:22 -0400

    Format command using awk (incomplete)

--- lastlog.go
+++ lastlog.go
@@ -6,9 +6,20 @@ import (
 )
 
 func lastlog() (string, error) {
-	users, err := exec.Command("/usr/bin/lastlog", "--time", "365").Output()
+	users := exec.Command("/usr/bin/lastlog", "--time", "365")
+	awk := exec.Command("/usr/bin/awk", `{print $1","$3","$4" "$5" "$6" "$7" "$8}`)
+
+	out, err := users.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
 	}
-	return string(users), nil
+	users.Start()
+	awk.Stdin = out
+
+	users_out, err := awk.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(users_out), nil
 }
