7410f75a2ab1ec6e05b094ed637632cb9ca263f8
Adel Qalieh <aqalieh95@gmail.com>
2014-10-03 16:14:53 -0400

    Format output of swaps using awk

--- swap.go
+++ swap.go
@@ -6,10 +6,20 @@ import (
 )
 
 func swap() (string, error) {
-	swaps, err := exec.Command("cat", "/proc/swaps").Output()
+	swap := exec.Command("cat", "/proc/swaps")
+	awk := exec.Command("awk", "{print $1,$2,$3,$4,$5}")
+
+	out, err := swap.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	swap.Start()
+	awk.Stdin = out
+
+	swap_out, err := awk.Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return string(swaps), nil
+	return string(swap_out), nil
 }
